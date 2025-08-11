485ecd34aa4a73444f07fafbd3c47f29b53a9ef2
Gabriel ML <gabrielmljob@gmail.com>
2025-08-11 23:56:45 +0200

    feat(TDD cycle: Refactor stage) Improve code & add JavaDoc
    - [x] Add JavaDoc documentation to class & methods.
    
    - [x] Ensure the list is created with all the days of the week.
    
    - [x] Returns a 'Defensive Copy' instead of a 'Direct Reference' to the
      internal list of days of the week.
    
      Return a 'Direct Reference' to the internal list can lead to
      'unintended side effects' & 'compromise the INTEGRITY of the object's
      state'. This violates the principle of ENCAPSULATION.
    
      Returning a 'Defensive Copy' provides complete independence. The
      returned list can be modified by the caller without affecting the
      internal list object. This ensure that the object's internal state
      remains under its own control, adhering to the princple of
      ENCAPSULATION.

--- src/main/java/dev/gml/DaysOfWeekManager.java
+++ src/main/java/dev/gml/DaysOfWeekManager.java
@@ -5,28 +5,42 @@
 package dev.gml;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Manages a list of the days of the week.
+ * It provides methods to 'create', 'retrieve', and 'manipulate' the list.
+ */
 public class DaysOfWeekManager {
-    // Attributes (aka fields)
+    // --- Attributes --- (aka fields)
     private List<String> daysOfWeek;
 
-    // Methods
+    // --- Methods ---
+    /**
+     * Initializes the list fo days of the week.
+     * The list is populated with the names of the seven days.
+     */
     public void createDaysOfWeekList() {
-        // Initialize list
-        daysOfWeek = new ArrayList<>();
-
-        // Add the days of the week;
-        this.daysOfWeek.add("Monday");
-        this.daysOfWeek.add("Tuesday");
-        this.daysOfWeek.add("Wednesday");
-        this.daysOfWeek.add("Thursday");
-        this.daysOfWeek.add("Friday");
-        this.daysOfWeek.add("Saturday");
-        this.daysOfWeek.add("Sunday");
+        // Create the list with all the days of the week.
+        daysOfWeek = new ArrayList<>(
+                Arrays.asList(
+                        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
     }
 
+    /**
+     * Returns the list of days of the week.
+     * 
+     * <p>
+     * This method now returns a '(!) Defensive Copy' of the internal list.
+     * The returned list can be modified freely by the caller without affecting
+     * the internal list of our object.
+     * </p>
+     * 
+     * @return An unmodifiable {@List List} of Strings containing the days of the
+     *         week.
+     */
     public List<String> getDaysOfWeek() {
-        return daysOfWeek;
+        return new ArrayList<>(daysOfWeek); // Return a copy to prevent external modification.
     }
 }
