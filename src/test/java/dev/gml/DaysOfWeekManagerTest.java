f66c371c1b64a13c528edfe4e6ea4a33f957731b
Gabriel ML <gabrielmljob@gmail.com>
2025-08-12 20:22:30 +0200

    feat(TDD cycle: Red stage): Add failing test to return requested day.
    This new test describes a desired behavior for the `DaysOfWeekManager`
    class: "It should return the correct day based on its index on the
    list".
    
    This test "should fail!", because the code to make it pass hasn't been
    written yet (`getDay()` method doesn't exist yet).

--- src/test/java/dev/gml/DaysOfWeekManagerTest.java
+++ src/test/java/dev/gml/DaysOfWeekManagerTest.java
@@ -6,7 +6,9 @@
 package dev.gml;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -73,5 +75,26 @@ public class DaysOfWeekManagerTest {
 
         // And I also assert that the list of days of the week doesn't contain the deleted day.
         assertThat(daysOfWeekManager.getDaysOfWeek(), not(hasItem("Monday")));
+    }
+
+    @Test
+    @DisplayName("4️⃣ It should return the correct day based on its index in the list.")
+    void getDayShouldReturnCorrectDayForGivenIndex(){
+        // --- Given ---
+        // The instance of DaysOfWeekManager that is initialized before each test,
+        // The index of the day I want to retrieve.
+        // The String name that corresponds to that day's index in the list.
+        int indexOfTheDay = 0;
+        String expectedDay = "Monday";
+
+        // --- When ---
+        // I create the list of days of the week.
+        // I retrieve a specific day.
+        daysOfWeekManager.createDaysOfWeekList();
+        String actualDay = daysOfWeekManager.getDay(indexOfTheDay);
+
+        // --- Then ---
+        // I can compare both names of the day of the week and say that they match.
+        assertThat(actualDay, is(equalTo(expectedDay)));
     }
 }
