d6947300225e15ede292c37e93c8a05cf970b44d
Gabriel ML <gabrielmljob@gmail.com>
2025-08-12 10:38:05 +0200

    refactor: Clean up `App.java` for `DaysOfWeekManager` demo.
    Removes unnecessary "Hello World" code and updates the Javadoc to
    accurately reflect the class's purpose as a demonstration of the
    `DaysOfWeekManager` class.
    
    This change improves clarity and focuses the `App.java` class on its
    role as entry point for observing the functionality of
    the `DaysOfWeekManager` class.

--- src/main/java/dev/gml/App.java
+++ src/main/java/dev/gml/App.java
@@ -1,17 +1,31 @@
+/**
+ * @file This is the main class for the 'DaysOfWeekManager' app.
+ * 
+ * This class servers as 'the entry point' to observe the 
+ * functionality of the 'DaysOfWeekManager'.
+ * 
+ * @author gml <kickAssDeveloper@hugsForMyBugs.mock>
+ */
 package dev.gml;
 
 /**
- * Hello world!
+ * 'Entry point' for the 'DaysOfWeekManager' application.
  */
 public final class App {
     private App() {
     }
 
     /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
+     * 'Main method' to demostrate the 'DaysOfWeekManager' functionality.
+     * 
+     * @param args The comand-lines arguments of the program (if necessary).
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        DaysOfWeekManager daysOfWeekManager = new DaysOfWeekManager();
+
+        daysOfWeekManager.createDaysOfWeekList();
+
+        System.out.println(daysOfWeekManager.getDaysOfWeek());
     }
 }
